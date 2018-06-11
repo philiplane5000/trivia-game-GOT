78aa067126308f8348825884820ffe0fa811c01c
philtd5000 <philiptaft9@gmail.com>
2018-06-11 11:41:44 -0700

    question countdown timer counts down, now to add conditionals to render new question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 console.log('CONNECTED');
 
+//https://www.fromthegrapevine.com/quizzes/arts/quiz-trivia-game-of-thrones
+
 let numOne = new QuestionGenerator("How did Daenerys Targaryen eventually hatch her dragon eggs?", "In a funeral pyre", "In a lightning storm", "In a fireplace", "In a frozen cave", "In a funeral pyre", "At the end of Season 1, Daenerys Targaryen placed her three dragon eggs on the funeral pyre of her late husband. She then walked into the flames and emerged from the ashes the next morning holding three newly hatched dragons.");
 let numTwo = new QuestionGenerator("Which U.S. city was one of 8 international locations visited by the 2015 'Game of Thrones' Exhibition?", "Chicago", "New York City", "San Diego", "Boston", "San Diego", "The 2015 San Diego Comic-Con played host to the 'Game of Thrones' Exhibition in the U.S. Over the next 12 months, the exhibit also visited Spain, Israel, France, England, Germany and Sweden.");
 let numThree = new QuestionGenerator("The phrase 'Valar Morghulis' or 'all men must die' is usually responded with:", "Valar Dohaeris or 'all men must serve'", "Valar Rohnas or 'all men must live'", "Valar GoGo or 'all men must dance'", "Valar Morgooglis or 'all must must dream'", "Valar Dohaeris or 'all men must serve'", "The Season 2 finale was named \"Valar Morghulis\" while the Season 3 premiere was named \"Valar Dohaeris.\" In 2014, the Brewery Ommegang created a beer called \"Valar Morghulis\", with each cork fire-branded with the response.");
@@ -10,12 +12,52 @@ let triviaArr = [numOne, numTwo, numThree, numFour, numFive];
 let correctTally = 0;
 let incorrectTally = 0;
 
-let timeRanOut = false;
+let questionTimerRunning = false;
+let moreInfoTimerRunning = false;
+let intervalIdOne;
+let intervalIdTwo;
 
 let questionTimerStart = 30; /*countdown timer to start at 30 upon page load */
 let moreInfoTimerStart = 10;
 let questionNum = 0; /*questionNumber = number that will determine which 'page'/question to render using renderQuestion and triviaQuestionArr array */
 
+let timer = {
+    questionTimer: 30,
+    moreInfoTimer: 10,
+
+    reset: function() {
+        timer.questionTimer = 30;
+        timer.moreInfoTimer = 10;    
+    },
+
+    startQuestionTimer: function() {
+        if (!questionTimerRunning) {
+            intervalIdOne = setInterval(timer.questionTimerCountdown, 1000);
+            questionTimerRunning = true;
+        }
+    },
+    startMoreInfoTimer: function() {
+        if (!moreInfoTimerRunning) {
+            intervalIdTwo = setInterval(timer.moreInfoTimerCountdown, 1000);
+            moreInfoTimerRunning = true;
+        }
+    },
+
+    questionTimerCountdown: function() {
+        /*if timer.questionTimer <= 0 increment page number and render new page*/
+        /*alternately, page click or guess will reset timer and render new page */
+        timer.questionTimer--;
+        let timeLeft = timer.questionTimer;
+        console.log(timeLeft);
+        $('#time-remaining').text(`${timeLeft}`);
+    }
+
+}
+
+
+function questionPageTimer(num) {
+
+}
 
 function renderQuestion(arrayOfQuestions, questionNumber) {
 
